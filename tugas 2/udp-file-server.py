3701921c5bb2ecf2fb4c8c4bc99878b00a761c8a
hafidsb <hafidsriwijaya98@gmail.com>
2019-03-18 21:53:12 +0700

    server berhenti otomatis

--- tugas 2/udp-file-server.py
+++ tugas 2/udp-file-server.py
@@ -2,20 +2,30 @@ import socket
 
 SERVER_IP = '127.0.0.1'
 SERVER_PORT = 5005
-NAMAFILE='data.jpg'
+NAMAFILE='new_data.jpg'
 
+# creates UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# binds just made socket to defined SERVER_PORT
 sock.bind((SERVER_IP, SERVER_PORT))
 
+# opens new file to be received
 fp = open(NAMAFILE,'wb+')
+
+# flag check if file has been received completely
 ditulis=0
 
 while True:
-	try:
-		data, addr = sock.recvfrom(1024)
-		print "blok ", len(data), data[0:10]
-		fp.write(data)
-	except KeyboardInterrupt:
-		data.close()
-	
+	recv_size = int(sock.recv(128))
+	data, addr = sock.recvfrom(1024)
+	print data
+	print "blok ", len(data), data[0:1]
+	fp.write(data)
+	ditulis = ditulis + 1
+
+	if recv_size == ditulis:
+		print "File sukses diterima."
+		break
+
 fp.close()
