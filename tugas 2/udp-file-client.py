8a72e3873cd0d7d6c8f90f76b5cf23cef59e156e
hafidsb <hafidsriwijaya98@gmail.com>
2019-03-18 18:44:07 +0700

    puz

---
+++ tugas 2/udp-file-client.py
@@ -0,0 +1,19 @@
+import socket
+import os
+
+TARGET_IP = "127.0.0.1"
+TARGET_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+namafile="small.jpg"
+ukuran = os.stat(namafile).st_size
+
+
+fp = open(namafile,'rb')
+k = fp.read()
+terkirim=0
+for x in k:
+	sock.sendto(x, (TARGET_IP, TARGET_PORT))
+	terkirim = terkirim + 1
+	print "\r terkirim {} of {} " . format(terkirim,ukuran)
