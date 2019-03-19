cbdc353268eb0c5d66286310b3a5fb73ee9ad7f5
hafidsb <hafidsriwijaya98@gmail.com>
2019-03-19 17:29:22 +0700

    Update udp-file-client.py

--- tugas 2/udp-file-client.py
+++ tugas 2/udp-file-client.py
@@ -9,11 +9,11 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 namafile="small.jpg"
 ukuran = os.stat(namafile).st_size
 
-fp = open(namafile,'rb')
-k = fp.read()
-terkirim=0
-for x in k:
-	sock.sendto(str(ukuran), (TARGET_IP, TARGET_PORT))
-	sock.sendto(x, (TARGET_IP, TARGET_PORT))
-	terkirim = terkirim + 1
-	print "\r terkirim {} of {} " . format(terkirim,ukuran)
+with open(namafile,'rb') as fp:
+	k = fp.read()
+	terkirim=0
+	for x in k:
+		sock.sendto(str(ukuran), (TARGET_IP, TARGET_PORT))
+		sock.sendto(x, (TARGET_IP, TARGET_PORT))
+		terkirim = terkirim + 1
+		print "\r terkirim {} of {} " . format(terkirim,ukuran)
