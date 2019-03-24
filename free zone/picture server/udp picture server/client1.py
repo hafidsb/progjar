7922653e59d14a9f33d9c7f87e986b5d82fbcd07
hafidsb <hafidsriwijaya98@gmail.com>
2019-03-25 00:26:38 +0700

    udp picture server uodate
    1-picture 1-client

--- free zone/picture server/udp picture server/client1.py
+++ free zone/picture server/udp picture server/client1.py
@@ -3,14 +3,48 @@ import socket
 # CONSTANTS
 TARGET_IP = 'localhost'
 TARGET_PORT = 9000
+FILE_NAME = 'download1.jpg'
 
 # create UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+# set client timeout
+sock.settimeout(5)
+
+# send connection request
 sock.sendto(("client").encode('utf8'), (TARGET_IP,TARGET_PORT))
 
-dari_server = sock.recv(128).decode('utf8')
+# accept connection response
+response_connection = sock.recv(128).decode('utf8')
+print(response_connection)
 
-print(dari_server)
+# send image request
+sock.sendto(("image").encode('utf8'), (TARGET_IP,TARGET_PORT))
 
-sock.close()
+# accept image response
+img_size = int(sock.recv(128).decode('utf8'))
+print(img_size)
+
+
+# open to be written file
+with open(FILE_NAME, 'wb+') as fo:
+    data_written = 0
+
+    while True:
+        try:            
+            img_data = sock.recv(64)
+            fo.write(img_data)
+            data_written += len(img_data)
+            print("\r Received {} of {}".format(data_written, img_size))
+            
+            if data_written == img_size:
+                print("Data received completely!")
+                sock.close()
+                break
+
+        except socket.timeout:
+            print("Client time out ..")
+            print("Shuting down client ..")
+            print(data_written, img_size)
+            sock.close()
+            break
--- free zone/picture server/udp picture server/server.py
+++ free zone/picture server/udp picture server/server.py
@@ -1,8 +1,10 @@
-import socket, threading
+import socket, os
 
 # CONSTANTS
 SERVER_IP = 'localhost'
 SERVER_PORT = 9000
+FILE_NAME = 'small.jpg'
+FILE_SIZE = os.stat(FILE_NAME).st_size
 
 # create UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -18,10 +20,28 @@ serverRunning = True
 while serverRunning:
     try:
         print("Waiting for connection ..")
-        data, client_addr = sock.recvfrom(128)
+        # accept connection request
+        request_connection, client_addr = sock.recvfrom(128)
         
-        if data.decode('utf8') == 'client':
-            sock.sendto(("halo dari server").encode('utf8'), client_addr)
+        if request_connection.decode('utf8') == 'client':
+            # send connection response
+            sock.sendto(("Request accepted").encode('utf8'), client_addr)
+        
+        # accept image request
+        request_image, client_addr = sock.recvfrom(128)
+
+        if request_connection.decode('utf8') == 'client':
+            # send image_size response
+            sock.sendto(str(FILE_SIZE).encode('utf8'), client_addr)
+
+        with open(FILE_NAME, 'rb') as fp:
+            data = fp.read()
+            data_sent = 0
+            
+            for x in data:
+                sock.sendto(x, client_addr)
+                data_sent += len(x)
+                print("\r Sent {} of {}".format(data_sent, FILE_SIZE))
 
     except socket.timeout:
         serverRunning = False
