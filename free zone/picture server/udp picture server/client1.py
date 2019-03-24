f02b27fd69b7c431b8fe44994b42afe10129ecac
hafidsb <hafidsriwijaya98@gmail.com>
2019-03-25 01:15:35 +0700

    update 1-client 2-picts

--- free zone/picture server/udp picture server/client1.py
+++ free zone/picture server/udp picture server/client1.py
@@ -4,6 +4,7 @@ import socket
 TARGET_IP = 'localhost'
 TARGET_PORT = 9000
 FILE_NAME = 'download1.jpg'
+FILE_NAME_2 = 'download2.jpg'
 
 # create UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -25,8 +26,7 @@ sock.sendto(("image").encode('utf8'), (TARGET_IP,TARGET_PORT))
 img_size = int(sock.recv(128).decode('utf8'))
 print(img_size)
 
-
-# open to be written file
+# first image handler
 with open(FILE_NAME, 'wb+') as fo:
     data_written = 0
 
@@ -38,13 +38,50 @@ with open(FILE_NAME, 'wb+') as fo:
             print("\r Received {} of {}".format(data_written, img_size))
             
             if data_written == img_size:
-                print("Data received completely!")
-                sock.close()
+                print("First image data received completely!")
+                print("Preparing for the 2nd image ..")
                 break
 
         except socket.timeout:
             print("Client time out ..")
             print("Shuting down client ..")
             print(data_written, img_size)
+            sock.close()
+            break
+# send 2nd connection request
+sock.sendto(("client2").encode('utf8'), (TARGET_IP,TARGET_PORT))
+
+# accept 2nd connection response
+response_connection = sock.recv(128).decode('utf8')
+print(response_connection)
+
+# send 2nd image request
+sock.sendto(("image2").encode('utf8'), (TARGET_IP,TARGET_PORT))
+
+# accept 2nd image response
+img_size2 = int(sock.recv(128).decode('utf8'))
+print(img_size2)
+
+# second image handler
+with open(FILE_NAME_2, 'wb+') as fo:
+    data_written = 0
+    
+    while True:
+        try:
+            img_data = sock.recv(64)
+            fo.write(img_data)
+            data_written += len(img_data)
+            print("\r Received {} of {}".format(data_written, img_size2))
+
+            if data_written == img_size2:
+                print("Secdond image data received completely!")
+                print("Exiting client ..")
+                sock.close()
+                print("Client exited successfully!")
+                break
+
+        except socket.timeout:
+            print("Client time out ..")
+            print("Shuting down client ..")
             sock.close()
             break
--- free zone/picture server/udp picture server/server.py
+++ free zone/picture server/udp picture server/server.py
@@ -3,8 +3,10 @@ import socket, os
 # CONSTANTS
 SERVER_IP = 'localhost'
 SERVER_PORT = 9000
-FILE_NAME = 'small.jpg'
-FILE_SIZE = os.stat(FILE_NAME).st_size
+FILE_NAME_1 = 'small.jpg'
+FILE_SIZE_1 = os.stat(FILE_NAME_1).st_size
+FILE_NAME_2 = 'spaghett.jpg'
+FILE_SIZE_2 = os.stat(FILE_NAME_2).st_size
 
 # create UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -30,18 +32,43 @@ while serverRunning:
         # accept image request
         request_image, client_addr = sock.recvfrom(128)
 
-        if request_connection.decode('utf8') == 'client':
+        if request_image.decode('utf8') == 'image':
             # send image_size response
-            sock.sendto(str(FILE_SIZE).encode('utf8'), client_addr)
+            sock.sendto(str(FILE_SIZE_1).encode('utf8'), client_addr)
 
-        with open(FILE_NAME, 'rb') as fp:
-            data = fp.read()
+        with open(FILE_NAME_1, 'rb') as fp:
+            data = fp.read(5)
             data_sent = 0
             
-            for x in data:
-                sock.sendto(x, client_addr)
-                data_sent += len(x)
-                print("\r Sent {} of {}".format(data_sent, FILE_SIZE))
+            while data:
+                sock.sendto(data, client_addr)
+                data_sent += len(data)
+                print("\r Sent {} of {}".format(data_sent, FILE_SIZE_1))
+                data = fp.read(5)
+
+        # accept 2nd connection request
+        request_connection, client_addr = sock.recvfrom(128)
+
+        if request_connection.decode('utf8') == 'client2':
+            # send 2nd connection response
+            sock.sendto(("Request accepted").encode('utf8'), client_addr)
+
+        # accept 2nd image request
+        request_image2, client_addr = sock.recvfrom(128)
+
+        if request_image2.decode('utf8') == 'image2':
+            # send image_size response
+            sock.sendto(str(FILE_SIZE_2).encode('utf8'), client_addr)
+        
+        with open(FILE_NAME_2, 'rb') as fp2:
+            data = fp2.read(5)
+            data_sent = 0
+            
+            while data:
+                sock.sendto(data, client_addr)
+                data_sent += len(data)
+                print("\r Sent {} of {}".format(data_sent, FILE_SIZE_1))
+                data = fp2.read(5)
 
     except socket.timeout:
         serverRunning = False
